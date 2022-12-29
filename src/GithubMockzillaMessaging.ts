779d6ccaba1f6eff5416680691a7784dcab3a6fa
kabeleced <kabeleced@arcor.de>
2022-12-29 17:11:02 +0100

    test: example implementation for unit test issues using mockzilla

---
+++ src/GithubMockzillaMessaging.ts
@@ -0,0 +1,14 @@
+import browser from 'webextension-polyfill'
+
+export class GithubMockzillaMessaging {
+  public connect(): void {
+    console.debug(`connect: start`)
+    browser.runtime.onConnect.addListener((port: browser.Runtime.Port) => {
+      console.debug(`onConnect: add listener`)
+      port.onMessage.addListener((message): void => {
+        console.debug(`onMessage: add listener: ${message}`)
+        port.postMessage(`${message}: reply`)
+      })
+    })
+  }
+}
---
+++ test/GithubMockzillaMessagingTest.test.ts
@@ -0,0 +1,37 @@
+import 'mockzilla-webextension'
+// import of mockzilla-webextension before(!) actual import of 'webextension-polyfill' in system-under-test
+import { mockEvent, MockzillaEventOf } from 'mockzilla-webextension'
+import { deepMock } from 'mockzilla'
+import { Runtime } from 'webextension-polyfill'
+import { GithubMockzillaMessaging } from './../src/GithubMockzillaMessaging'
+
+describe('BackgroundScriptMessaging', () => {
+  const [port, mockPort, mockPortNode] = deepMock<Runtime.Port>('port')
+  let msgs = new Array()
+  let addListenerOnConnect: MockzillaEventOf<typeof mockBrowser.runtime.onConnect>
+  let addListenerOnMessage: MockzillaEventOf<typeof mockPort.onMessage>
+
+  beforeEach(() => {
+    addListenerOnConnect = mockEvent(mockBrowser.runtime.onConnect)
+    addListenerOnMessage = mockEvent(mockPort.onMessage)
+  })
+
+  describe('MessagingBackgroundScript function test', () => {
+    it('method connect() shall handle connection request', async () => {
+      mockPort.postMessage.expect('test-message: reply')
+      const sut = new GithubMockzillaMessaging()
+      sut.connect()
+      addListenerOnConnect.emit(port)
+      addListenerOnMessage.emit('test-message', port)
+    })
+  })
+  describe('MessagingBackgroundScript function test 2', () => {
+    it('method connect() shall handle more connection requests', async () => {
+      mockPort.postMessage.expect('test-message: reply')
+      const sut = new GithubMockzillaMessaging()
+      sut.connect()
+      addListenerOnConnect.emit(port)
+      addListenerOnMessage.emit('test-message-2', port)
+    })
+  })
+})
