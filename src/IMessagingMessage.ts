a520c4ca909016220de095edf69c6f1fc9ed8897
kabeleced <kabeleced@arcor.de>
2023-04-14 21:30:00 +0200

    docs: description extended

--- src/IMessagingMessage.ts
+++ src/IMessagingMessage.ts
@@ -4,6 +4,10 @@ import { IMessagingMessageName } from './IMessagingMessageName'
  * Interface describing a serializable message sent via a messaging system.
  *
  * A messaging message must have at least a name for identification.
+ *
+ * A message without any content - except for the name - can be used to trigger an action on the receiving site.
+ * 
+ * In order to sent actual content a sub-type of this interface can be used.
  */
 export interface IMessagingMessage {
   name: IMessagingMessageName
