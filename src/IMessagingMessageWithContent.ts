7f3fa5d0d7acc0c7d2b81e49dc567ea82590d792
kabeleced <kabeleced@arcor.de>
2023-04-15 14:37:16 +0200

    docs: extended description

--- src/IMessagingMessageWithContent.ts
+++ src/IMessagingMessageWithContent.ts
@@ -1,9 +1,9 @@
 import { IMessagingMessage } from './IMessagingMessage'
 
 /**
- * Interface of a message sent via a messaging system.
+ * Interface of a message sent via a messaging system including content.
  *
- * A messaging message has a certain type and transports a generic content.
+ * A messaging message with content extends the base messaging-message by a generic content.
  */
 export interface IMessagingMessageWithContent<TContent> extends IMessagingMessage {
   content: TContent
