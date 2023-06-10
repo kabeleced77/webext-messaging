9c08a212f9b3e9dc54cbe0f52cdac53a336e1c09
kabeleced <kabeleced@arcor.de>
2023-06-10 19:25:52 +0200

    refactor: added error handling to  MessagingContentScript()

--- src/MessagingContentScript.ts
+++ src/MessagingContentScript.ts
@@ -3,16 +3,22 @@ import { IMessagingSend } from './IMessagingSend'
 import { IMessagingPort } from './IMessagingPort'
 
 export class MessagingContentScript<TSend, TReceive> implements IMessagingSend<TSend, TReceive> {
-  constructor(private port: IMessagingPort) {}
+  constructor(private port: IMessagingPort) { }
 
   public send(message: TSend): Promise<TReceive> {
     return new Promise((resolve, reject) => {
       try {
+        if (this.port === undefined) throw new Error("Messaging port is undefined.")
+        if (this.port.name === undefined) throw new Error("Messaging port does not implement method 'name()'.")
+        if (this.port.name() === undefined) throw new Error("Messaging port's name is undefined.")
+        // connect to opened listeners (e.g. from background script)
         const port = browser.runtime.connect({ name: this.port.name() })
+        // send message (e.g. to background script)
+        port.postMessage(message)
+        // register handler in case a reply is sent (e.g. from background script)
         port.onMessage.addListener((m, p): void => {
           resolve(m)
         })
-        port.postMessage(message)
       } catch (error) {
         reject(error)
       }
--- test/MessagingContentScript.test.ts
+++ test/MessagingContentScript.test.ts
@@ -18,7 +18,7 @@ describe('MessagingContentScript', () => {
   afterEach(() => mockPortNode.verifyAndDisable())
 
   describe('MessagingContentScript function test', () => {
-    it('expect no reply of sent message', async () => {
+    it('expect no reply of sent (message !== undefined)', async () => {
       const portName = 'port-name'
       const message = 'message-no-reply'
       const mockedMessagingPort = new Mock<IMessagingPort>()
@@ -30,7 +30,19 @@ describe('MessagingContentScript', () => {
       const sut = new MessagingContentScript<string, void>(mockedMessagingPort)
       sut.send(message)
     })
-    it('expect a reply of sent message', async () => {
+    it('expect no reply of sent (message === undefined)', async () => {
+      const portName = 'port-name'
+      const message = undefined!
+      const mockedMessagingPort = new Mock<IMessagingPort>()
+        .setup((port) => port.name())
+        .returns(portName)
+        .object()
+      mockBrowser.runtime.connect.expect({ name: portName }).andReturn(port)
+      mockPort.postMessage.expect(message).times(1)
+      const sut = new MessagingContentScript<string, void>(mockedMessagingPort)
+      sut.send(message)
+    })
+    it('expect a (reply !== undefined) of sent (message !== undefined)', async () => {
       const portName = 'port-name'
       const message = 'message-with-reply'
       const messageReply = 'reply of message-with-reply'
@@ -43,6 +55,78 @@ describe('MessagingContentScript', () => {
       const sut = new MessagingContentScript<string, string>(mockedMessagingPort)
       sut.send(message).then(reply => expect(reply).toEqual(messageReply))
       addListenerOnMessage.emit(messageReply, port)
+    })
+    it('expect a (reply !== undefined) of sent (message === undefined)', async () => {
+      const portName = 'port-name'
+      const message = undefined!
+      const messageReply = 'reply of message-with-reply'
+      const mockedMessagingPort = new Mock<IMessagingPort>()
+        .setup((port) => port.name())
+        .returns(portName)
+        .object()
+      mockBrowser.runtime.connect.expect({ name: portName }).andReturn(port)
+      mockPort.postMessage.expect(message).times(1)
+      const sut = new MessagingContentScript<string, string>(mockedMessagingPort)
+      sut.send(message).then(reply => expect(reply).toEqual(messageReply))
+      addListenerOnMessage.emit(messageReply, port)
+    })
+    it('expect a (reply === undefined) of sent (message !== undefined)', async () => {
+      const portName = 'port-name'
+      const message = 'message-with-reply'
+      const messageReply = undefined
+      const mockedMessagingPort = new Mock<IMessagingPort>()
+        .setup((port) => port.name())
+        .returns(portName)
+        .object()
+      mockBrowser.runtime.connect.expect({ name: portName }).andReturn(port)
+      mockPort.postMessage.expect(message).times(1)
+      const sut = new MessagingContentScript<string, string>(mockedMessagingPort)
+      sut.send(message).then(reply => expect(reply).toEqual(messageReply))
+      addListenerOnMessage.emit(messageReply, port)
+    })
+    it('expect a (reply !== undefined) of sent (message === undefined)', async () => {
+      const portName = 'port-name'
+      const message = undefined!
+      const messageReply = undefined
+      const mockedMessagingPort = new Mock<IMessagingPort>()
+        .setup((port) => port.name())
+        .returns(portName)
+        .object()
+      mockBrowser.runtime.connect.expect({ name: portName }).andReturn(port)
+      mockPort.postMessage.expect(message).times(1)
+      const sut = new MessagingContentScript<string, string>(mockedMessagingPort)
+      sut.send(message).then(reply => expect(reply).toEqual(messageReply))
+      addListenerOnMessage.emit(messageReply, port)
+    })
+    it('expect error if messaging-port-interface is not implementing method name()', async () => {
+      const message = 'message-no-reply'
+      const mockedMessagingPort = undefined!
+      expect(() => {
+        const sut = new MessagingContentScript<string, void>(mockedMessagingPort)
+        sut.send(message)
+      }).rejects
+    })
+    it('expect error if messaging-port-interface is not implementing method name()', async () => {
+      const message = 'message-no-reply'
+      const mockedMessagingPort = new Mock<IMessagingPort>()
+        .setup((port) => port.name)
+        .returns(undefined!)
+        .object()
+      expect(() => {
+        const sut = new MessagingContentScript<string,void>(mockedMessagingPort)
+        sut.send(message)
+      }).rejects
+    })
+    it('expect error if messaging-port-interface is not implemented', async () => {
+      const message = 'message-no-reply'
+      const mockedMessagingPort = new Mock<IMessagingPort>()
+        .setup((port) => port.name())
+        .returns(undefined!)
+        .object()
+      expect(() => {
+        const sut = new MessagingContentScript<string, void>(mockedMessagingPort)
+        sut.send(message)
+      }).rejects
     })
   })
 })
