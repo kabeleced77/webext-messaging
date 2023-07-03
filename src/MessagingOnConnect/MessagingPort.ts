1ec329714a75d38cba5f9ce0a7434d4c8b9d35da
kabeleced <kabeleced@arcor.de>
2023-07-03 21:52:38 +0200

    fix: implementation of interface IMessagingPort

--- src/MessagingOnConnect/MessagingPort.ts
+++ src/MessagingOnConnect/MessagingPort.ts
@@ -1,8 +1,8 @@
 import { IMessagingPort } from './IMessagingPort'
 
-export class MessagingPort implements IMessagingPort {
+export class MessagingPort implements IMessagingPort<string> {
   constructor(private readonly portName: string) {}
-  public name(): string {
+  public id(): string {
     return this.portName
   }
 }
