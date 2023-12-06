9e18f4bfa76c65226e5e49952dda134bfe7089d0
kabeleced <kabeleced@arcor.de>
2023-12-06 20:54:03 +0100

    refactor: removed unused example-files

--- test/MessagingOneOff/GithubOneOffMessaging.ts
+++
@@ -1,9 +0,0 @@
-import browser from 'webextension-polyfill'
-
-export default class OneOffMessaging<TReceive, TReply> {
-  public handle(
-    handleMessage: (message: TReceive, sender?: browser.Runtime.MessageSender) => Promise<TReply>,
-  ): void {
-    browser.runtime.onMessage.addListener(handleMessage)
-  }
-}
